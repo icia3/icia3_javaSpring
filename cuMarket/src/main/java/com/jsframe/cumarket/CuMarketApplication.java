91f4222f2c737e09aa11de1690d6dae7c39c5a73
army-17 <j2665138@naver.com>
2022-11-10 11:22:41 +0900

    frame

---
+++ cuMarket/src/main/java/com/jsframe/cumarket/CuMarketApplication.java
@@ -0,0 +1,13 @@
+package com.jsframe.cumarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuMarketApplication.class, args);
+	}
+
+}
---
+++ cuMarket/src/test/java/com/jsframe/cumarket/CuMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsframe.cumarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
