992f7596cb2c40e426df5bc9f578d0241f4cec82
army-17 <j2665138@naver.com>
2022-11-10 14:52:34 +0900

    total entity

---
+++ cuMarket/src/main/java/com/jsframe/cumarket/entity/Comment.java
@@ -0,0 +1,26 @@
+package com.jsframe.cumarket.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "commenttbl")
+@Data
+public class Comment {
+
+    //댓글 번호
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long rnum;
+
+    //작성자 아이디
+    @ManyToOne
+    @JoinColumn(name = "cid")
+    private Member rid;
+
+    //댓글 내용
+    @Column(nullable = false, length = 100)
+    private String reply;
+
+}
