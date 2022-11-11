0abaa20bf45831494293f1d938fafbffd444bec4
1600owner <thfl5464@gmail.com>
2022-11-11 10:49:40 +0900

    service

--- cuMarket/src/main/java/com/jsframe/cumarket/controller/Controller.java
+++ cuMarket/src/main/java/com/jsframe/cumarket/controller/Controller.java
@@ -1,12 +1,20 @@
 package com.jsframe.cumarket.controller;
 
+import com.jsframe.cumarket.repository.BoardRepository;
+import com.jsframe.cumarket.serivce.Service;
 import lombok.extern.java.Log;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @org.springframework.stereotype.Controller
 @Log
 public class Controller {
+    ModelAndView mv;
+
+    @Autowired
+    private Service Serv;
 
     @GetMapping("/")
     public String home(){
@@ -24,5 +32,14 @@ public class Controller {
     public String join(){
         log.info("join()");
         return "join";
+    }
+
+    @GetMapping("detail")
+    public ModelAndView detail(long bnum){
+        log.info("detail()");
+        mv = new ModelAndView();
+        mv = Serv.getBoard(bnum);
+        mv.setViewName("detail");
+        return mv;
     }
 }
