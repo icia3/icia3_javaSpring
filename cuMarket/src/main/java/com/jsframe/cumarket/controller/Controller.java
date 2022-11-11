2e87f8ed736259a311f66a68f9b3301a616a7dba
army-17 <j2665138@naver.com>
2022-11-11 10:52:06 +0900

    login

--- cuMarket/src/main/java/com/jsframe/cumarket/controller/Controller.java
+++ cuMarket/src/main/java/com/jsframe/cumarket/controller/Controller.java
@@ -14,15 +14,19 @@ public class Controller {
         return "home";
     }
 
+    @GetMapping("join")
+    public String join(){
+        log.info("join()");
+        return "join";
+    }
+
     @GetMapping("login")
     public String login(){
         log.info("login");
         return "login";
     }
 
-    @GetMapping("join")
-    public String join(){
-        log.info("join()");
-        return "join";
-    }
+
+
+
 }
